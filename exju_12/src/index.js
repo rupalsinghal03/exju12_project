8dbb475dba336dff0de66cf2bb781197c93ca459
rupalsinghal03 <109016450+rupalsinghal03@users.noreply.github.com>
2022-07-23 13:39:38 +0530

    Add files via upload

---
+++ exju_12/src/index.js
@@ -0,0 +1,78 @@
+import React,{useState} from "react";
+import ReactDOM from "react-dom";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+import "./styles.css";
+
+const renderTime = ({ remainingTime }) => {
+  
+  if (remainingTime === 0) {
+    return <div className="timer">Too lale...</div>;
+  }
+
+  return (
+    <div className="timer">
+      <div className="text">Remaining</div>
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds</div>
+    </div>
+  );
+};
+
+const options = [10, 20, 30, 40, 50];
+
+const SetlectReset = ({onClickReset}) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <select>
+        {options.map((opt) => (
+          <option value={opt} key={opt}>
+            {opt + " sec"}
+          </option>
+        ))}
+      </select>
+      <button onClick={onClickReset}>Reset</button>
+    </div>
+  );
+};
+
+function App() {
+  const [key, setKey] = useState(0);
+  const onClickReset = () => {
+    setKey(prevKey => prevKey + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        <a
+          href="https://www.reactlabs.ai/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Labs
+        </a>
+        <br />
+        ReactJS Assignment
+      </h1>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          key={key}
+          isPlaying
+          duration={10}
+          colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+        >
+          {renderTime}
+        </CountdownCircleTimer>
+        <SetlectReset onClickReset={onClickReset}/>
+        <h3>Can you get this reset button to work?</h3>
+        <h4>
+          The reset button should reset as per the time selected in the dropdown
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
